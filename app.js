a2f231613ade37659704a9c4e8f2892b528c5423
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-11-10 20:59:45 +0200

    add

---
+++ app.js
@@ -0,0 +1,12 @@
+// ? Task:
+
+
+// Solution 1
+
+document.addEventListener("DOMContentLoaded",function(){
+	this.forms[0].addEventListener("change",e => {
+		e.target.removeAttribute("class");
+	});
+});
+
+// ! Explanation: 
